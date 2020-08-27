1336587fc713572a9fda29c9a8a87911cf55437b
Prakash Shrestha <prakashs@uab.edu>
2020-08-27 13:24:35 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<img class="size-medium wp-image-124 alignright" src="http://vorlon.cs.uab.edu/prakashs/wp-content/uploads/sites/8/2016/11/profile-272x300.png" alt="" width="272" height="300" />I am a Post-Doctoral Researcher at the <a href="https://www.ufl.edu/">University of Florida</a> working in the areas related to IoT security and privacy with <a href="http://www.daniela.ece.ufl.edu/Home.html">Dr. Daniela Oliveira</a>.
+
+<span class="fontstyle0">My research spans across the areas of security and privacy in/using computing devices, machine learning, the human aspect of cybersecurity, human-computer interaction, and mobile and wearable sensible user behavior. My dissertation explored new authentication/deauthentication and privacy paradigms, from both offensive and defensive perspectives, in/using mobile, wearable, and IoT computing leveraging various embedded sensors (e.g., audio sensors, such as microphones and speakers, and motion-position sensors, such as accelerometer and gyroscope). My research articles have been published at various top security venues, including CCS, NDSS, WiSec, and ACM-CSUR and received considerable attention at major news outlets, such as Forensic Magazine, Science Daily, IT Security News, TechXplore, Naked Security, Webroot, Scientific Computing, Helpnet Security, SD Times, ECN, etc. </span>
+
+<em>I obtained my Ph.D. in Computer Science from the  <a href="http://www.uab.edu/home/">University of Alabama at Birmingham (UAB)</a>, USA, and a Bachelor's degree in Computer Engineering from <a href="http://www.tribhuvan-university.edu.np/">Tribhuvan University</a>, Nepal. </em>
+
+My <a href="http://vorlon.cs.uab.edu/prakashs/wp-content/uploads/sites/8/2020/05/CV_Prakash.pdf">Curriculum Vitae</a> and <a href="http://vorlon.cs.uab.edu/prakashs/wp-content/uploads/sites/8/2020/05/Prakash_Research_Statement-1.pdf">Research Statement</a>.
+
+&nbsp;
+
+&nbsp;
+
